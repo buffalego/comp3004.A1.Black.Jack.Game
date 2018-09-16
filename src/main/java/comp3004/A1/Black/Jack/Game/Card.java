f3be2ffef92a849c29207c9c2b35a7b7f73e6325
BuffaLego <BuffaLego@tos-MacBook-Pro.local>
2018-09-16 16:33:12 -0400

    Added Card class

--- src/main/java/comp3004/A1/Black/Jack/Game/Card.java
+++ src/main/java/comp3004/A1/Black/Jack/Game/Card.java
@@ -2,10 +2,29 @@ package comp3004.A1.Black.Jack.Game;
 
 public class Card 
 {
+	// create enum constants of Suit
+	public enum Suit {
+		S, C, D, H
+	}
+	// create enum constants of Value
+	public enum Value 
+	{
+		TWO ("2"), THREE ("3"), FOUR ("4"), FIVE ("5"), SIX ("6"), SEVEN ("7"), EIGHT ("8")), NINE ("9"), TEN ("10"), JACK ("J"), QUEEN ("Q"), KING ("K"), ACE ("A")
+		
+		private String val;
+		   public String getVal() {
+		       return this.val;
+		   }
+		   Value(String val) {
+		           this.val = val;
+		   }
+		}
+	}
 
 	private Suit suit;
 	private Value value;
 	
+	// constructs for Card
 	public Card(Suit suit, Value value)
 	{
 		this.suit = suit;
