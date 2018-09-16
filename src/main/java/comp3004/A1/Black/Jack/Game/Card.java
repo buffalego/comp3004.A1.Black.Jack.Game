232e3212421eb0893cc4f96bf6eb5c3c9439e3c9
BuffaLego <BuffaLego@tos-MacBook-Pro.local>
2018-09-16 16:01:01 -0400

    Added Card class

---
+++ src/main/java/comp3004/A1/Black/Jack/Game/Card.java
@@ -0,0 +1,14 @@
+package comp3004.A1.Black.Jack.Game;
+
+public class Card 
+{
+
+	private Suit suit;
+	private Value value;
+	
+	public Card(Suit suit, Value value)
+	{
+		this.suit = suit;
+		this.value = value;
+	}
+}
