fc07544b48c0b4a6fc6868ca4ed9c9078d06bdb9
BuffaLego <BuffaLego@tos-MacBook-Pro.local>
2018-09-17 00:23:07 -0400

    added playingDeck, dealerCards, and playerCards

--- src/main/java/comp3004/A1/Black/Jack/Game/BlackJackStart.java
+++ src/main/java/comp3004/A1/Black/Jack/Game/BlackJackStart.java
@@ -5,6 +5,20 @@ public class BlackJackStart
 	public static void main(String[] args)
 	{
 		System.out.println("Let's Play!");
+		
+		//create new deck for dealer
+		//add to make a full deck
+		//then shuffle it
+		Deck dealingDeck = new Deck();
+		
+		dealingDeck.makeFullDeck();
+		dealingDeck.mixDeck();
+		
+		//create a hand of cards for dealer
+		Deck dealerCards = new Deck();
+		
+		//create a hand of cards for player
+		Deck playerCards = new Deck();
 	}
 
 }
