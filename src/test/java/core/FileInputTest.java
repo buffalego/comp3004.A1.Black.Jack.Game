e5518cf55cee6bc9bef4ebfb253abe5a3529e5bb
BuffaLego <BuffaLego@tos-MacBook-Pro.local>
2018-09-17 01:42:14 -0400

    Added FileInput test

---
+++ src/test/java/core/FileInputTest.java
@@ -0,0 +1,11 @@
+package core;
+
+import java.util.Scanner;
+
+import junit.framework.TestCase;
+
+public class FileInputTest extends TestCase
+{
+	Scanner playerInput = new Scanner(System.in);
+	
+}
