6bb0f27290e55ed227452fe88187b30f6fb9da02
BuffaLego <BuffaLego@tos-MacBook-Pro.local>
2018-09-16 22:28:43 -0400

    Modified DeckCount test

--- src/test/java/core/DeckCountTest.java
+++ src/test/java/core/DeckCountTest.java
@@ -7,7 +7,7 @@ import junit.framework.TestCase;
 
 public class DeckCountTest extends TestCase {
 	
-
+	//check if deck contains 52 cards or not
 	public void testDectCount() {
 		Deck cards = new Deck();
 		cards.createFullDeck();
@@ -19,19 +19,6 @@ public class DeckCountTest extends TestCase {
 
 
 /*
- List<String> deck = 
- 
-{"SA","S2","S3","S4","S5","S6","S7","S8","S9","S10","SJ","SQ","SK",
- "CA","C2","C3","C4","C5","C6","C7","C8","C9","C10","CJ","CQ","CK",
- "DA","D2","D3","D4","D5","D6","D7","D8","D9","D10","DJ","DQ","DK",
- "HA","H2","H3","H4","H5","H6","H7","H8","H9","H10","HJ","HQ","HK",	
-}
-
-need functions:
-countCard();
-getCard();
-getSuit();
-getRank();
 
 
 https://dzone.com/articles/using-java-enums
