edaa24c1651d5f7922c6c6fe3ccd899b9458290e
BuffaLego <BuffaLego@tos-MacBook-Pro.local>
2018-09-16 22:24:33 -0400

    Added getValue and toString functions in Card class

--- src/main/java/comp3004/A1/Black/Jack/Game/Card.java
+++ src/main/java/comp3004/A1/Black/Jack/Game/Card.java
@@ -30,4 +30,18 @@ public class Card
 		this.suit = suit;
 		this.value = value;
 	}
+	
+	public Value getValue()
+	{
+		return this.value;
+	}
+	
+	public String toString(){
+		return this.suit.toString() + "." + this.value.toString();
+	}
+	
+	
+	
+	
+	
 }
