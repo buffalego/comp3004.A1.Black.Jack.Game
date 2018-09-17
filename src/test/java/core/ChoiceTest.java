02bfb108b48b35e9ac806f0c73d9da583ad181a2
BuffaLego <BuffaLego@dhcp-32-30.cu-wireless-faculty.carleton.ca>
2018-09-17 10:18:09 -0400

    Added Choice test

---
+++ src/test/java/core/ChoiceTest.java
@@ -0,0 +1,20 @@
+package core;
+
+import java.util.Scanner;
+
+public class ChoiceTest extends TestCase
+{
+	public void testFileChoice()
+	{
+		//get player choice
+		Scanner playerInput = new Scanner(System.in);
+		String select = playerInput.nextLine();
+		
+		if (select == "input" || select == "i")
+		{
+			
+		}
+		
+		
+	}
+}
