0022a13a7fa8bde6dad42c87d94a834203f8b50e
BuffaLego <BuffaLego@tos-MacBook-Pro.local>
2018-09-19 23:40:25 -0400

    Modified cardsValue in Deck class

--- src/main/java/comp3004/A1/Black/Jack/Game/Deck.java
+++ src/main/java/comp3004/A1/Black/Jack/Game/Deck.java
@@ -22,9 +22,11 @@ public class Deck
 	{
 		//Add Cards to the Deck
 		//Loop Through Suits
-		for(Suit cardSuit : Suit.values()){
+		for(Suit cardSuit : Suit.values())
+		{
 			//Loop through Ranks
-			for(Rank cardValue : Rank.values()){
+			for(Rank cardValue : Rank.values())
+			{
 				//Add new card to the mix
 				this.cards.add(new Card(cardSuit,cardValue));
 			}
@@ -33,7 +35,8 @@ public class Deck
 	
 	public int getSize(){ return this.cards.size(); }
 
-	public void mixDeck() {
+	public void mixDeck() 
+	{
 		// TODO Auto-generated method stub
 		Collections.shuffle(cards);
 	    isMixed = true;
@@ -58,26 +61,32 @@ public class Deck
 	public void addCard(Card addCard){ this.cards.add(addCard); }
 	
 	// take all the cards on player and dealer hands back to the deck
-	public void movePlayingHandsToDeck(Deck dealedDeck){
-		int thisDeckSize = this.cards.size();
+	public void movePlayingHandsToDeck(Deck dealedDeck)
+	{
+		int thisHandSize = this.cards.size();
 		
 		//put cards on hand back into dealed deck
-		for(int i = 0; i < thisDeckSize; i++){
+		for(int i = 0; i < thisHandSize; i++)
+		{
 			dealedDeck.addCard(this.getCard(i));
 		}
 		//empty out the hand
-		for(int i = 0; i < thisDeckSize; i++){
+		for(int i = 0; i < thisHandSize; i++)
+		{
 			this.removeCard(0);
 		}
 	}
 	
-	public int cardsValue(){
+	public int cardsValue()
+	{
 		int totalValue = 0;
 		int aces = 0;
-		//For every card in the deck
+		//loop for every card in the dealing deck
 		for(Card aCard : this.cards){
+			
 			//Switch of possible values
-			switch(aCard.getValue()){
+			switch(aCard.getValue())
+			{
 			case TWO: totalValue += 2; break;
 			case THREE: totalValue += 3; break;
 			case FOUR: totalValue += 4; break;
@@ -91,10 +100,31 @@ public class Deck
 			case QUEEN: totalValue += 10; break;
 			case KING: totalValue += 10; break;
 			case ACE: aces += 1; break;
-			
-			
 			}
 		}
+
+		//Determine the total value of aces
+		//Aces worth 11 or 1 - if 11 would go over 21 make it worth 1
+		//int thisHandSize = this.cards.size();
+		
+		for(int i = 0; i < aces; i++)
+		{	
+			//if a hand has 2 or more ace and total value without aces is more than 10
+			if (totalValue > 10 && aces > 1 && aces <=4)
+			{
+				totalValue += 1; // one ace = 1
+			}
+			//if a hand has 2 or more aces and total value without aces is less than 10
+			else if (totalValue < 10 && aces > 1 && aces <=4)
+			{
+				// first plus 11 to total for first ace value then plus (aces - 1) to total value ==> (aces - 1) worth 1 each
+				totalValue = totalValue + 11 + (aces -1);
+			}
+			
+		}
+		//Return
+		return totalValue;
+	}
 	
 	
 	
