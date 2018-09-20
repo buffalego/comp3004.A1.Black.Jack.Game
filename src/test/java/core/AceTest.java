2eb8bedf6ae1a1e2315397e372651c278ec77198
BuffaLego <BuffaLego@tos-MacBook-Pro.local>
2018-09-19 21:51:47 -0400

    Added cardsValue() to Deck class

--- src/test/java/core/AceTest.java
+++ src/test/java/core/AceTest.java
@@ -1,5 +1,11 @@
 package core;
 
-public class AceTest {
+import comp3004.A1.Black.Jack.Game.Card;
+import junit.framework.TestCase;
 
+public class AceTest extends TestCase {
+	public void aceValue()
+	{
+		
+	}
 }
